89dc5394242efd65ef67556cefd94f79332e9583
Rado <radekspam5@gmail.com>
2019-03-02 17:55:14 +0100

    start

---
+++ src/test/java/com/radek/webapp/WebAppApplicationTests.java
@@ -0,0 +1,20 @@
+package com.radek.webapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAppApplicationTests {
+
+	@Test //comenstadssadadssssszsass
+	public void contextLoads() {
+		Assert.assertTrue(true);
+	}
+
+	//git add -A && git commit -m "msg" && git push origin development
+
+}
