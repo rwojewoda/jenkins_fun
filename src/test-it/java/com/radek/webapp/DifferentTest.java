dd007b14eb1ca16f3efb0513c2f1a5f82edef81e
Rado <radekspam5@gmail.com>
2019-03-03 19:13:07 +0100

    msg

--- src/test-it/java/com/radek/webapp/DifferentTest.java
+++ src/test-it/java/com/radek/webapp/DifferentTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DifferentTest {
 
-	@Test //comenstadssadadssssszsaas
+	@Test //comenstadssadadssssszsaass
 	public void contextLoads() {
 		System.out.print("Dziala!");
 		Assert.assertTrue(true);
