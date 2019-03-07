fe484c98918c8e9967d4234794183681529ccf76
Rado <radekspam5@gmail.com>
2019-03-07 18:44:21 +0100

    msg

--- src/test/java/com/radek/webapp/WebAppApplicationTests.java
+++ src/test/java/com/radek/webapp/WebAppApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WebAppApplicationTests {
 
-	@Test //comenstadssadadssssszsaasdsss
+	@Test //comenstad
 	public void contextLoads() {
 		Assert.assertTrue(true);
 	}
